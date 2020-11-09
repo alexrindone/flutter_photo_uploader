e9b2a8e66382895fefc2f93979f9f642ebfc06f9
Alex Rindone <arindone@iapp.org>
2020-11-08 22:03:30 -0500

    clearing out tests for later

--- test/photo_uploader_test.dart
+++ test/photo_uploader_test.dart
@@ -1,9 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:photo_uploader/photo_uploader.dart';
 
-void main() {
-  test('adds one to input values', () {
+void main() async {
 
-  });
 }
